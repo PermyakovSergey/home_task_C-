61c498f1b325f73e780cb4e540789ff793da4e10
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-08-06 04:00:36 +0500

    added solution for task_3

--- seminar_1_task_3/Program.cs
+++ seminar_1_task_3/Program.cs
@@ -3,3 +3,13 @@
 //4 -> да
 //-3 -> нет
 //7 -> нет
+
+Console.WriteLine("Напишите число, я выдам является ли число чётным");
+
+Console.Write("введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+    Console.WriteLine("да");
+if (number % 2 != 0)
+    Console.WriteLine("нет");
