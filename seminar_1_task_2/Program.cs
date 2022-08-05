441fc97c854105392f0a9e45845d2b22dc497dc3
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-08-06 03:39:52 +0500

    added solution for task_2

--- seminar_1_task_2/Program.cs
+++ seminar_1_task_2/Program.cs
@@ -3,3 +3,23 @@
 //2, 3, 7 -> 7
 //44 5 78 -> 78
 //22 3 9 -> 22
+
+Console.WriteLine("Напишите три числа, я выдам какое из них максимальное(max)");
+
+Console.Write("введите первое число: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("введите второе число: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("введите третье число: ");
+double c = double.Parse(Console.ReadLine());
+
+double max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
