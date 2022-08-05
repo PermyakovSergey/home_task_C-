40b825a656b884e54252720a0fdbb7dbe231fd3d
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-08-06 04:34:11 +0500

    added solution for task_4

--- seminar_1_task_4/Program.cs
+++ seminar_1_task_4/Program.cs
@@ -2,3 +2,15 @@
 
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число > 1, я выдам все чётные числа от 1 до N");
+
+Console.Write("введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 0;
+if (number <= 1)
+    Console.WriteLine("неверное число");
+while (count < number - 1)
+    {count += 2;
+    Console.WriteLine(count);}
