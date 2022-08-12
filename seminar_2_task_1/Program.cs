fad332e6a510b3ad45ec74755385d9b98764eb27
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-08-12 21:01:07 +0500

    added task 1 decision

--- seminar_2_task_1/Program.cs
+++ seminar_2_task_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число, программа покажет вторую цифру этого числа: ");
+string number = Convert.ToString(int.Parse(Console.ReadLine()));
+Console.WriteLine("Введено число: " + number);
+Console.WriteLine("Вторая цифра числа: " + number[1]);
