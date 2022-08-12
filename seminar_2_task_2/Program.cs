a8479a96c3b3f6110c840d78db08a05dcf6479a5
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-08-12 21:32:54 +0500

    added task 2 decision

--- seminar_2_task_2/Program.cs
+++ seminar_2_task_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Write("Введите число, программа покажет третью цифру этого числа: ");
+string number = Convert.ToString(int.Parse(Console.ReadLine()));
+if (number.Length > 2)
+{
+    Console.WriteLine("Третья цифра числа: " + number[2]);
+}
+if (number.Length <= 2)
+{
+    Console.WriteLine("Во введенном числе отсутствует третья цифра");
+}
