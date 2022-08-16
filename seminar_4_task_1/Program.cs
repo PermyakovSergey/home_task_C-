3abe0f44d91702e5afbceff8a415e2cc66963a5f
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-08-16 21:08:22 +0500

    added decision for hometask 1 of seminar 4

---
+++ seminar_4_task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double degree(double x, int y)
+{
+    double result = x;
+    for (int i = 1; i < y; i++)
+    {
+        result = result * x;
+    }
+    return result;
+}
+Console.WriteLine("Введите два числа, первое будет возведено в степень равную второму числу");
+Console.WriteLine("Введите первое число: ");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Число {a} в степени {b} равно: {degree(a, b)}");
