60484e19880cd88645ba4c2c1121b900c40ce8ac
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-08-16 12:31:42 +0500

    added decision for task1

--- seminar_3_task_1/Program.cs
+++ seminar_3_task_1/Program.cs
@@ -2,3 +2,40 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.Write("Enter an integer to check if it is a palindrome or not: ");
+string number = Console.ReadLine();
+
+string mirror = string.Empty;
+
+for (int index = 0; index < number.Length; index++)
+{
+    char temp = number[index];
+    mirror = mirror + number[number.Length - index -1];
+}
+if (number == mirror)
+
+Console.WriteLine("Entered integer is a palindrome");
+
+if (number != mirror)
+
+Console.WriteLine("Entered integer is NOT a palindrome");
+
+// Console.Write("Enter an integer to check if it is a palindrome or not: ");
+// string number = Console.ReadLine();
+// string mirror = string.Empty;
+// int index = 0;
+// int count = number.Length;
+// while (index < count)
+// {
+//     char temp = number[index];
+//     mirror = mirror + number[number.Length - index -1];
+//     index++;
+// }
+// if (number == mirror)
+
+// Console.WriteLine("Entered integer is a palindrome");
+
+// if (number != mirror)
+
+// Console.WriteLine("Entered integer is NOT a palindrome");
