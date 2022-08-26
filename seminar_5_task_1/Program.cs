90aefca7d4b0cac5459935eb823f0da03493f0f5
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-08-26 11:10:48 +0500

    added decision for hometask 1 of seminar 5

--- seminar_5_task_1/Program.cs
+++ seminar_5_task_1/Program.cs
@@ -2,3 +2,28 @@
 // которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+int[] GenArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(99, 1000);
+    }
+    Console.WriteLine(string.Join(",", arr));
+    return arr;
+}
+void SearchEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    Console.WriteLine($"Number of even numbers in the array: = {count}");
+}
+
+Console.WriteLine("Please enter a size of massive: ");
+int size = int.Parse(Console.ReadLine());
+
+int[] array = GenArray(size);
+SearchEvenNumbers(array);
