e070b8e01d8ae4a0f926a99551940c34e6dd0eaa
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-08-26 11:17:20 +0500

    added decision for hometask 2 of seminar 5

--- seminar_5_task_2/Program.cs
+++ seminar_5_task_2/Program.cs
@@ -2,3 +2,33 @@
 // стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] GenArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    Console.WriteLine(string.Join(",", arr));
+    return arr;
+}
+
+void SummOddNumbers(int[] array)
+{
+    int summoddnumbers = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0) summoddnumbers += array[i];
+    }
+    Console.WriteLine($"The summ of odd numbers in the array: = {summoddnumbers}");
+}
+Console.WriteLine("Please enter a size of massive: ");
+int size = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter minimum value of massive: ");
+int min = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter maximum value of massive: ");
+int max = int.Parse(Console.ReadLine());
+int[] array = GenArray(size, min, max);
+
+SummOddNumbers(array);
