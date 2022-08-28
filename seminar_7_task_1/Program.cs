766ae9ad8995b7528ec25ea640d568dcf7e86871
PermyakovSergey <permyakov.s.84@yandex.ru>
2022-08-29 04:02:54 +0500

    added decisions for HT of seminar 7

--- seminar_7_task_1/Program.cs
+++ seminar_7_task_1/Program.cs
@@ -4,3 +4,72 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+int CheckInputForArraySize()
+{
+    Console.Write("it must be number > 0: ");
+    string input = Console.ReadLine();
+
+    if (!int.TryParse(input, out int number))
+    {
+        Console.Write("Value can't be text, ");
+        return CheckInputForArraySize();
+    }
+    if (number == 0 || number < 0)
+    {
+        Console.Write("Value can't be negative or zero, ");
+        return CheckInputForArraySize();
+    }
+    return number;
+}
+
+int CheckInputIfNotNumber()
+{
+    Console.Write("enter a number: ");
+    string input = Console.ReadLine();
+
+    if (!int.TryParse(input, out int number))
+    {
+        Console.Write("Value can't be text, ");
+        return CheckInputIfNotNumber();
+    }
+    return number;
+}
+
+double[,] GenRandomArray()
+{
+    Console.Write("Enter the number of rows in the array, ");
+    int rows = CheckInputForArraySize();
+    Console.Write("Enter the number of columns in the array, ");
+    int columns = CheckInputForArraySize();
+    Console.Write("Enter the minimum value of elements in array: ");
+    int min = CheckInputIfNotNumber();
+    Console.Write("Enter the maximum value of elements in array: ");
+    int max = CheckInputIfNotNumber();
+    double[,] randomarray = new double[rows, columns];
+    if (min > max) {int temp = min; min = max; max = temp;}
+    if (min == max) 
+    {Console.WriteLine("Since the entered minimum and maximum values are equal, the maximum is increased by one");}
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            randomarray[i, j] = Math.Round(new Random().Next(min, max) + new Random().NextDouble(), 1);
+        }
+    }
+    return randomarray;
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array = GenRandomArray();
+PrintArray(array);
--- seminar_7_task_2/Program.cs
+++ seminar_7_task_2/Program.cs
@@ -6,3 +6,64 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
+int CheckInputIfNotNumber()
+{
+    Console.Write("enter a number: ");
+    string input = Console.ReadLine();
+
+    if (!int.TryParse(input, out int number))
+    {
+        Console.Write("Value can't be text, ");
+        return CheckInputIfNotNumber();
+    }
+    return number;
+}
+
+int[,] GenRandomArray()
+{
+    int[,] randomarray = new int[3, 4];
+    for (int i = 0; i < randomarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < randomarray.GetLength(1); j++)
+        {
+            randomarray[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return randomarray;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumberInArray(int[,] arr) // Метод выводит индексы всех найденных значений
+{
+    Console.Write("For searching in the array ");
+    int numberfind = CheckInputIfNotNumber();
+    int count = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] == numberfind)
+            {
+                Console.WriteLine($"The number {numberfind} was found in the position with the index [{i}, {j}] ");
+                count++;
+            }
+        }
+    }
+    if (count == 0) {Console.WriteLine($"The number {numberfind} is not in the array");}
+}
+
+Console.WriteLine("An array of numbers is given");
+int[,] array = GenRandomArray();
+PrintArray(array);
+FindNumberInArray(array);
